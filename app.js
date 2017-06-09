88dbe57dcbdd7fc1438cb1b7b0f2b531f1f0ed02
Will Russell <whiskeyromeo06@gmail.com>
2017-06-09 11:53:11 -0400

    add favicon

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
