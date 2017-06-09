22fd9565f24a562de4fe7e391d2562edbbc485cb
Will Russell <whiskeyromeo06@gmail.com>
2017-06-09 11:08:08 -0400

    first deploy beach2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
